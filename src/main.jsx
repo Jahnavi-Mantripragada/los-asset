7700a21cbc0f53c9bf83c30891eab895c0f69024
Yashwanth <agentsdr83@gmail.com>
2026-05-19 16:12:27 +0530

    added both modes for app sync

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,26 @@ import amplifyConfig from "./amplifyConfig.js";
 
 import "./index.css";
 
-Amplify.configure(amplifyConfig);
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: "ap-south-1_uDfL65aJf",
+      userPoolClientId: "4sb1q16bm23tdlp3b1naoi08s6",
+    },
+  },
+
+  API: {
+    GraphQL: {
+      endpoint:
+        "https://oqjf2oe72nedtpg3nuomr4655e.appsync-api.ap-south-1.amazonaws.com/graphql",
+      region: "ap-south-1",
+
+      defaultAuthMode: "userPool", // IMPORTANT FIX
+
+      apiKey: "da2-pyivlzc6dfgg7bdu4wdjnld32i", // fallback (keep this)
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
